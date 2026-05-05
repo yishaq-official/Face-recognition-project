32e82057bce8c22eb08ef65442ece6591311006e
yishaq-official <yishaq.official@gmail.com>
2026-05-05 09:49:45 +0300

    new employee enrolled for test

--- backend/app.py
+++ backend/app.py
@@ -59,4 +59,5 @@ def video_feed():
 
 if __name__ == '__main__':
     print("[*] Starting Flask-SocketIO server on http://localhost:5000")
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    # Set use_reloader=False to prevent the double-start issue with webcams
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000, use_reloader=False)
