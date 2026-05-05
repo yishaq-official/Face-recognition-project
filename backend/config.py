6510f5b2a5877dbfd0a95bed3c2bac685b27bc46
yishaq-official <yishaq.official@gmail.com>
2026-05-05 09:52:37 +0300

    separation of database config file to remove hardcoding.

---
+++ backend/config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+    DB_NAME = "aegis_pentagon_db"
+    UPLOAD_FOLDER = os.path.join("static", "uploads")
+    # Strict matching threshold (lower is stricter)
+    TOLERANCE = 0.5
